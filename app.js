c260f6db6d105a73d49cb35eb7c548b5017d0272
vuthinh08 <vuvanthinh2k1@gmail.com>
2022-08-26 09:57:06 +0700

    fix select css

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const submitBtn = document.getElementById('submit');
-const rated = document.getElementById('rating');
-const thankElement = document.querySelector('.card-thank')
-const mainElement = document.querySelector('.card-main');
-const ratings = document.querySelectorAll('.btn');
+const submitBtn = document.getElementById("submit");
+const rated = document.getElementById("rating");
+const thankElement = document.querySelector(".card-thank");
+const mainElement = document.querySelector(".card-main");
+const ratings = document.querySelectorAll(".btn");
 
 ratings.forEach((rating) => {
-    rating.onclick = () => {
-        rated.innerHTML = rating.innerHTML;
-    }
-})
+  rating.onclick = () => {
+    rating.classList.add("is-selected");
+    rated.innerHTML = rating.innerHTML;
+  };
+});
 
 submitBtn.onclick = () => {
-    thankElement.classList.toggle('hidden');
-    mainElement.classList.toggle('hidden');
-    // ratings.forEach((rating) => {
-    //     console.log(rating)
-    //     rating.onclick = () => {
-    //         console.log(rating.innerHTML)
-    //         rated.innerHTML = rating.innerHTML;
-    //     }
-    // })
-}
+  thankElement.classList.toggle("hidden");
+  mainElement.classList.toggle("hidden");
 
+  // ratings.forEach((rating) => {
+  //     console.log(rating)
+  //     rating.onclick = () => {
+  //         console.log(rating.innerHTML)
+  //         rated.innerHTML = rating.innerHTML;
+  //     }
+  // })
+};
